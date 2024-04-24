a5f0a7819d038c6269594380062a551a02afa7dc
Ignacio Anaya <ignacio.anaya89@gmail.com>
2024-04-23 23:05:38 -0300

    docs: add new examples

--- examples/public-subnets-only/main.tf
+++ examples/public-subnets-only/main.tf
@@ -7,9 +7,7 @@ module "eks_clickhouse" {
 
   install_clickhouse_operator = true
   install_clickhouse_cluster  = true
-
-  # Set to true if you want to use a public load balancer (and expose ports to the public Internet)
-  clickhouse_cluster_enable_loadbalancer = false
+  eks_enable_nat_gateway      = false
 
   eks_cluster_name = "clickhouse-cluster"
   eks_region       = local.region
@@ -19,11 +17,6 @@ module "eks_clickhouse" {
     "${local.region}a",
     "${local.region}b",
     "${local.region}c"
-  ]
-  eks_private_cidr = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24"
   ]
   eks_public_cidr = [
     "10.0.101.0/24",
